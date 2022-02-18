ae4ad4d5ee17b98dc0af68ff87daed7dd6f3cda7
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-02-18 11:11:57 +0200

    Make Button more sassy

--- src/Components/Button.svelte
+++ src/Components/Button.svelte
@@ -6,10 +6,12 @@
     {title}
 </button>
 
-<style>
+<style lang="scss">
+
+    @import '../styles/colors.scss';
     button {
-        background-color: var(--button-color);
-        color:var(--white);
+        background-color: $button-color;
+        color: $white;
         cursor: pointer;
         border-radius: 10px;
         padding: 11px 0;
@@ -20,14 +22,14 @@
     }
 
     button:hover {
-	    filter: brightness(1.2);
-        transition: ease filter 0.2; 
+	    background-color: darken($color: $button-color, $amount: 10%);
+        transition: ease background-color 0.2; 
 
     }
 
     button:active {
-        filter: brightness(1.3);
-        transition: ease filter 0.5; 
+        background-color: darken($color: $button-color, $amount: 20%);
+        transition: ease background-color 0.2; 
 
     }
 
