c0acfc49a322228375df7be602459146f5a35a6b
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-02-18 11:19:37 +0200

    Card component skeleton

---
+++ src/Components/Card.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Button from './components/Button.svelte';
+</script>
+
+<div class="card">
+	<slot name="title"></slot>
+	<Button $title />
+</div>
+
+<style lang="scss">
+
+    @import '../styles/colors.scss';
+
+    .card{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
