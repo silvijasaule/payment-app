e0a3c3caada162836141bb01fad73ae0e0d1c222
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-02-20 11:06:29 +0200

    Create reusable Card component

---
+++ src/Components/atoms/Card.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class="card">
+    <slot></slot>
+</div>
+
+<style lang="scss">
+
+    @import '../../styles/colors.scss';
+
+    .card{
+        align-items: center;
+        background-color: rgba($color: $white, $alpha: 0.6);
+        border-radius: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 60px;
+    }
+</style>
