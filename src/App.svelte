eb96d1c0d705eb44d12e9ed68628db00e3667262
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-03-10 13:17:32 +0200

    Add one more else if block

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	import HelloCard from "./Components/organisms/HelloCard.svelte";
 	import ClaimCard from "./Components/organisms/ClaimCard.svelte";
-	// import AddBalanceCard from "./Components/organisms/AddBalanceCard.svelte";
+	import AddBalanceCard from "./Components/organisms/AddBalanceCard.svelte";
 	import NotAuthorizedCard from "./Components/organisms/NotAuthorizedCard.svelte";
 	import { userAddress, userConnected } from './stores/Network.js';
 
@@ -20,6 +20,8 @@
 		<HelloCard/>
 	{:else if $userConnected && $userAddress == "0x73774102b7a588b31ed43d79903ced2d48b543e3"}
 		<ClaimCard/>
+	{:else if $userConnected && $userAddress == "0xC370b50eC6101781ed1f1690A00BF91cd27D77c4"}
+		<AddBalanceCard/>
 	{:else}
 		<NotAuthorizedCard/>	
 	{/if}
