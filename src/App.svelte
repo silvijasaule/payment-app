8e744e5c75d1e730cbabcc72caa1ea40a3574182
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-03-10 15:26:01 +0200

    Check chain ID

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,8 @@
 	import ClaimCard from "./Components/organisms/ClaimCard.svelte";
 	import AddBalanceCard from "./Components/organisms/AddBalanceCard.svelte";
 	import NotAuthorizedCard from "./Components/organisms/NotAuthorizedCard.svelte";
-	import { userAddress, userConnected } from './stores/Network.js';
+	import ConnectToBinanceCard from "./Components/organisms/ConnectToBinanceCard.svelte";
+	import { userAddress, userConnected, chainID } from './stores/Network.js';
 
 	const beneficiary = "0x73774102B7A588B31ED43d79903Ced2d48B543e3";
 	const owner = "0xC370b50eC6101781ed1f1690A00BF91cd27D77c4";
@@ -12,6 +13,8 @@
 <main>
 	{#if !$userConnected}
 		<HelloCard/>
+	{:else if $userConnected && $chainID != "0x38"}
+		<ConnectToBinanceCard/>
 	{:else if $userConnected && $userAddress == beneficiary}
 		<ClaimCard/>
 	{:else if $userConnected && $userAddress == owner}
---
+++ src/Components/organisms/ConnectToBinanceCard.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Card from '../atoms/Card.svelte';
+	import Button from '../atoms/Button.svelte';
+    import { connectWallet } from '../../stores/Network.js';
+</script>
+
+<Card>
+	<h1 class="card__title"> ❗ Wrong network</h1>
+    <h3 class="card__subtitle">It seems that you are connected to wrong network. 
+        Please connect to Binance Smart Chain.</h3>
+	<Button title={"Connect wallet"} onClick={connectWallet} />
+</Card>
+
+<style lang="scss">
+
+    @import '../../styles/colors.scss';
+
+    .card{
+
+        &__title {
+            color: $text-color;
+            font-size: 2.25em;
+            margin:0 0 2em;
+        }
+
+        &__subtitle {
+            color:rgba($text-color, 0.8);
+            font-size: 1.125em;
+            margin: 0 0 3em;
+            font-weight: 500;
+            white-space: pre-line;
+            text-align: center;
+        }
+    }
+</style>
